926ed19adafd1030bda42bffa1a65ee2ff3b639d
Anthony <anthony.marque@gmail.com>
2019-05-13 11:11:19 +0200

    updated apiKey and firebase creds

--- optimizing-lazy-loading/src/app/app.component.ts
+++ optimizing-lazy-loading/src/app/app.component.ts
@@ -11,8 +11,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     firebase.initializeApp({
-      apiKey: "AIzaSyDGDTtGNC4FkKpSe85qb2wLHzNi2xkxFy8",
-      authDomain: "ng-recipe-book.firebaseapp.com"
+      apiKey: "AIzaSyCcInDlBNMagcYyYILXBgRsoivrmrzppbo",
+      authDomain: "recipe-4f1fb.firebaseapp.com"
     });
   }
 
